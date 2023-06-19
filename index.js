01c6f42a3cfd6eb915c8a20d74749a8e08ed138f
Your Name <you@example.com>
2023-06-19 17:18:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function openEmail() {
+    // window.open(String('mailto:test^example.com').repeat('^','@'));
+    let gmail = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=kruticpawar11@gmail.com'
+    location.href = gmail;
+    
+  }
